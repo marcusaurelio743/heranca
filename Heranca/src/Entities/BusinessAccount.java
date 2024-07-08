b5261f1349e511f479a1f05d8bd67e94d59048fc
marcus aurelio <marcusaurelio743@gmail.com>
2024-07-08 17:26:07 -0300

    conceito de herança

---
+++ Heranca/src/Entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package Entities;
+
+public class BusinessAccount extends Account {
+	private Double LoanLimit;
+	
+	BusinessAccount(){
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		LoanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return LoanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		LoanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= LoanLimit) {
+			balance -= amount -10.00;
+		}
+	}
+	
+
+}
