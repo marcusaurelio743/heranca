495a6bb6c3224821ff788afac75a869373c8b4aa
marcus aurelio <marcusaurelio743@gmail.com>
2024-07-12 22:00:20 -0300

    tratando exeções

--- Heranca/src/ex/Aplication/Program.java
+++ Heranca/src/ex/Aplication/Program.java
@@ -7,23 +7,36 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-			Scanner sc = new Scanner(System.in);
-			
-			try {
-				String[] vect = sc.nextLine().split(" ");
-				int position = sc.nextInt();
-				System.out.println(vect[position]);
-			}
-			catch(ArrayIndexOutOfBoundsException e) {
-				System.out.println("invalid Position");
-			}
-			catch(InputMismatchException e){
-				System.out.println("Invalid Value");
-			}
-			
-			System.out.println("end for Program");
+				method1();
+				System.out.println("end for Program");
 		
+		
+	}
+	
+	public static void method1() {
+		System.out.println("Method 1 start");
+		method2();
+		System.out.println("Method 1 end");
+	}
+	
+	public static void method2() {
+		System.out.println("Method 2 start");
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("invalid Position");
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid Value");
+		}
+
 		sc.close();
+		
+		System.out.println("Method 2 end");
+
 	}
 
 }
